6413e1ef9fe7b47ea8d544429b9aa60968531004
iumag <iumag@yandex.ru>
2017-03-19 16:02:13 +0800

    Show Error if item event not change

--- AleEvent/src/pages/event/event.ts
+++ AleEvent/src/pages/event/event.ts
@@ -1,13 +1,13 @@
-﻿import { Component, OnInit} from '@angular/core';
-import { HttpService} from '../../app/http.service';
+﻿import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../app/http.service';
 import { CartService } from '../../app/cart.service';
 import { RelationService } from '../../app/relations.service';
-import { NavController, ModalController, Platform, NavParams, ViewController, MenuController } from 'ionic-angular';
-import { Response} from '@angular/http';
+import { NavController, ModalController, Platform, NavParams, ViewController, MenuController, AlertController } from 'ionic-angular';
+import { Response } from '@angular/http';
 import { Event } from '../../app/event';
-import {ModalContentPage} from '../../modal/modal';
+import { ModalContentPage } from '../../modal/modal';
 import { Cart } from '../../app/cart';
-import {HotelPage} from  '../hotel/hotel';
+import { HotelPage } from '../hotel/hotel';
 @Component({
     selector: 'page-event',
     templateUrl: 'event.html'
@@ -21,7 +21,7 @@ export class EventPage {
     holiday_id: Number;
     show_button: boolean;
 
-    constructor(public menu: MenuController, public navCtrl: NavController, private httpService: HttpService, public modalCtrl: ModalController,private cartService: CartService,  private relationService : RelationService) {
+    constructor(public alertCtrl: AlertController, public menu: MenuController, public navCtrl: NavController, private httpService: HttpService, public modalCtrl: ModalController, private cartService: CartService, private relationService: RelationService) {
 
     }
 
@@ -38,30 +38,38 @@ export class EventPage {
     }
 
     selectEvent(item) {
-            item.show = !item.show;
-            this.show_button = true;
-            if(item.show === false) {
-                this.cartService.deleteCart(item.id, 'event');
-                this.events.filter(this.isShow).length > 0 ? this.show_button = true : this.show_button = false
-                return;
-            }
-           this.cartService.createCart(item.id,'event', item);
-           console.log(this.cartService.getCart());
+        item.show = !item.show;
+        this.show_button = true;
+        if (item.show === false) {
+            this.cartService.deleteCart(item.id, 'event');
+            return;
+        }
+        this.cartService.createCart(item.id, 'event', item);
+        console.log(this.cartService.getCart());
     }
 
-    isShow(value){
+    isShow(value) {
         return value.show === true
     }
 
-    openHotel(){
+    openHotel() {
+        if (this.events.filter(this.isShow).length <= 0) {
+            let alert = this.alertCtrl.create({
+                title: 'Error',
+                subTitle: 'Choose item!',
+                buttons: ['OK']
+            });
+            alert.present();
+            return;
+        }
         this.navCtrl.push(HotelPage);
     }
 
     onLink(url: string) {
         window.open(url);
     }
-    
-    openMenu(){
+
+    openMenu() {
         this.menu.open();
     }
 }
